7ad4d2921bb16dbc1ab796a8c773d2d230b6f37d
minsub_labtop <04min12@naver.com>
2026-03-01 12:14:14 +0900

    연속되는 그룹문자의 수를 찾는 문제. 알파벳 배열을 만들어 이미 나온 알파벳을 걸러내는 방식을 이전에 배운 문제들에서 생각해냈다. 하지만 새 줄이 시작될 때 문자배열과 알파벳배열 모두 다시 초기화해야한다는 사실을 놓치고 좀 애먹었다.

--- Project1/project1.c
+++ Project1/project1.c
@@ -1,39 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-char A[1000001] = " ";
-
+#include <stdbool.h>
+#include <string.h>
+char A[101] = " ";
+int alphabet[26] = { 0 };
 int main(void) {
-	scanf("%s", A);
-	int count = 0;
-	for (int i = 0; A[i] != '\0'; i++) {
 
-		if (A[i] == 'c' && A[i + 1] == '=')
+	int count =0;
+	int n;
+	scanf("%d", &n);
+	
 
-			i++; // 다다음 인덱스로 점프
-		else if (A[i] == 'c' && A[i + 1] == '-') i++;
-		else if (A[i] == 'd' && A[i + 1] == 'z' && A[i + 2] == '=') {
+	for (int i = 0; i < n; i++) {
+		bool isgroup = true;
+		A[0] = '\0';
+		memset(alphabet, 0, sizeof(alphabet));
+		scanf("%s", A);
+		for (int j = 0; A[j] != '\0'; j++) {
+			
+			if (alphabet[(int)A[j] - 'a'] != 0) {
+				isgroup = false; // 2. 해당 인덱스가 이미 나온적 있으면 그룹단어가 아님
+				break;
+			}
+			if (A[j] != A[j + 1]) { // 1. 만약 그 다음 인덱스에 다른 문자가 왔는데
+				alphabet[(int)A[j] - 'a']++; // (해당 알파벳 이미 나왔음 alphabet[i] 가 1이라면 이미 나왔단 얘기)
 
-			i = i + 2; // 다다다음 으로 점프
+			}
+
+
 		}
-		else if (A[i] == 'd' && A[i + 1] == '-') {
-
-			i++;
-		}
-		else if (A[i] == 'l' && A[i + 1] == 'j') i++;
-		else if (A[i] == 'n' && A[i + 1] == 'j') i++;
-		else if (A[i] == 'n' && A[i + 1] == 'j') i++;
-		else if (A[i] == 's' && A[i + 1] == '=') i++;
-		else if (A[i] == 'z' && A[i + 1] == '=') i++;
-		count++;
+		if(isgroup)
+			count++;
+		
 	}
-
 	printf("%d", count);
+	
 }
-/* č	c=
-ć	c-
-dž	dz=
-đ	d-
-lj	lj
-nj	nj
-š	s=
-ž	z= */
+//만약 그 다음 인덱스에 다른 문자가 왔는데 또 같은 문자를 받는다면 그룹단어가 아님
